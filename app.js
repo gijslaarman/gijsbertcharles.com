2fdca9cdc11b5ad66ce39b7053171199bb84bbcd
gijslaarman <gijslaarman@live.nl>
2020-05-01 16:17:33 +0200

    req.headers + req.body

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static('pages'))
 app.disable('x-powered-by')
 
 app.post('/update-website', (req, res) => {
-    console.log(req)
+    console.log(req.headers, req.body)
     console.log('======')
     build(function() {
         res.send('Updated website!')
